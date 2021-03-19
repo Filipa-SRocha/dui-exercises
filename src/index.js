e655942ee0cdb0bf836bbaf385220f228a75afb2
Filipa-SRocha <filipa.sr2@gmail.com>
2021-03-19 21:45:33 +0000

    added event listeners and navigation functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+import { displayImage, nextImage } from './modules/handleImg';
 import { load } from './modules/loadPage';
 
 load();
+
+const Dom = (function () {
+	const arrows = document.querySelectorAll('.arrows');
+	const dots = document.querySelectorAll('.all-dots');
+
+	arrows.forEach((arrow) =>
+		arrow.addEventListener('click', function (e) {
+			nextImage(this.id);
+		})
+	);
+
+	dots.forEach((dot) =>
+		dot.addEventListener('click', function (e) {
+			displayImage(this.dataset.index);
+		})
+	);
+})();
--- src/modules/handleImg.js
+++ src/modules/handleImg.js
@@ -27,14 +27,30 @@ function appendImages() {
 	imageArray.forEach((image) => imagesDiv.appendChild(image));
 }
 
-function displayImage(img) {
+function displayImage(index) {
 	imageArray.forEach((otherImg) => {
 		if (otherImg.classList.contains('displaying')) {
 			otherImg.classList.toggle('displaying');
 		}
 	});
 
-	img.classList.toggle('displaying');
+	imageArray[index].classList.toggle('displaying');
 }
 
-export { loadImages, appendImages, displayImage };
+function getDisplayedImage() {
+	const currentImg = document.querySelector('.displaying');
+	return parseInt(currentImg.dataset.index);
+}
+
+function nextImage(direction) {
+	const index = getDisplayedImage();
+	console.log(index);
+	let newIndex = direction == 'arrow-left' ? index - 1 : index + 1;
+
+	if (newIndex < 0) newIndex = 4;
+	else if (newIndex > 4) newIndex = 0;
+
+	displayImage(newIndex);
+}
+
+export { loadImages, appendImages, displayImage, nextImage };
